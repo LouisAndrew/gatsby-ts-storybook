f602caadb6b89ef9493dffc6a9acedc74084830c
Louis Andrew <louisandrew3@gmail.com>
2020-06-23 13:44:28 +0200

    streamlined.

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,59 +1,53 @@
-import React from "react";
-import styled, { ThemeProvider } from "styled-components";
-import { useStaticQuery, graphql } from "gatsby";
-import { theme, GlobalStyles } from "../../styles";
+import React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import { useStaticQuery, graphql } from 'gatsby'
+import { theme, GlobalStyles } from '../../styles'
 // Components
-import { CSSDebugger } from "../css-debugger";
-import { Link } from "../link";
-import { Footer } from "../footer";
+import { CSSDebugger } from '../css-debugger'
 
 const Container = styled.div`
-  margin: 0 auto;
-  max-width: 1080px;
-  padding: 2rem;
-`;
+        margin: 0 auto;
+        max-width: 1080px;
+        padding: 2rem;
+`
 
 const Title = styled.h1`
-  font-size: ${props => (props.theme.screens.sm ? "1.8rem" : "2.2rem")};
-  margin: 20px 0px;
-  color: white;
-`;
+        font-size: ${props => (props.theme.screens.sm ? '1.8rem' : '2.2rem')};
+        margin: 20px 0px;
+        color: white;
+`
 
 const Tagline = styled.h2`
-  font-size: 1.1rem;
-  font-weight: 400;
-  color: ${props => props.theme.colors.blue};
-`;
+        font-size: 1.1rem;
+        font-weight: 400;
+        color: ${props => props.theme.colors.blue};
+`
 
 const Layout: React.FC = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-          description
-        }
-      }
-    }
-  `);
+        const data = useStaticQuery(graphql`
+                query SiteTitleQuery {
+                        site {
+                                siteMetadata {
+                                        title
+                                        description
+                                }
+                        }
+                }
+        `)
 
-  const { title, description } = data.site.siteMetadata;
+        const { title, description } = data.site.siteMetadata
 
-  return (
-    <ThemeProvider theme={theme()}>
-      <Container>
-        <GlobalStyles />
-        <CSSDebugger />
-        <Link to="/">
-          <Title>{title.toUpperCase()}</Title>
-        </Link>
-        <Tagline>{description}</Tagline>
-        <br />
-        <main>{children}</main>
-        <Footer />
-      </Container>
-    </ThemeProvider>
-  );
-};
+        return (
+                <ThemeProvider theme={theme()}>
+                        <Container>
+                                <GlobalStyles />
+                                <CSSDebugger />
+                                <Tagline>{description}</Tagline>
+                                <br />
+                                <main>{children}</main>
+                        </Container>
+                </ThemeProvider>
+        )
+}
 
-export { Layout };
+export { Layout }
