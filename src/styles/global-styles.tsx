2adc5acfcdcd8a8c5eae9af619b021640aa6fe5e
Louis Andrew <louisandrew3@gmail.com>
2020-08-01 19:21:29 +0200

    updated configs.

--- src/styles/global-styles.tsx
+++ src/styles/global-styles.tsx
@@ -1,15 +1,11 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    background: ${props => props.theme.colors.darkblue}; 
-    color: white;
-  }
-  html {
-    font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  }
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
 `;
 
 export { GlobalStyles };
